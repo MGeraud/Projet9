562cc4e874a7a021cd4c1bdcf64002c5628f3faa
MGeraud <geraud.mon@gmail.com>
2021-01-06 06:05:36 +0100

    Add tests to ComptabiliteDaoImplTest

--- src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -1,7 +1,9 @@
 package com.dummy.myerp.consumer.dao.impl.db.dao;
 
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
+import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.technical.exception.NotFoundException;
 import com.dummy.myerp.testconsumer.consumer.ConsumerTestCase;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,25 +19,41 @@ public class ComptabiliteDaoImplTest extends ConsumerTestCase {
     @Test
     public void getListCompteComptable() {
         List<CompteComptable> compteComptableList = getDaoProxy().getComptabiliteDao().getListCompteComptable();
-        Assert.assertNotNull(compteComptableList);
+        assertFalse(compteComptableList.isEmpty());
     }
 
     @Test
     public void getListJournalComptable() {
         List<JournalComptable> journalComptableList = getDaoProxy().getComptabiliteDao().getListJournalComptable();
-        Assert.assertNotNull(journalComptableList);
+        assertFalse(journalComptableList.isEmpty());
     }
 
     @Test
     public void getListEcritureComptable() {
+        List<EcritureComptable> ecritureComptableList = getDaoProxy().getComptabiliteDao().getListEcritureComptable();
+        assertFalse(ecritureComptableList.isEmpty());
     }
 
     @Test
-    public void getEcritureComptable() {
+    public void getEcritureComptable() throws NotFoundException {
+        EcritureComptable ecritureComptable = getDaoProxy().getComptabiliteDao().getEcritureComptable(-1);
+        assertEquals("AC-2016/00001" , ecritureComptable.getReference());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getEcritureComptableByIdNotFound() throws NotFoundException {
+        EcritureComptable ecritureComptable = getDaoProxy().getComptabiliteDao().getEcritureComptable(8);
     }
 
     @Test
-    public void getEcritureComptableByRef() {
+    public void getEcritureComptableByRef() throws NotFoundException {
+        EcritureComptable ecritureComptable = getDaoProxy().getComptabiliteDao().getEcritureComptableByRef("BQ-2016/00003");
+        assertEquals("Paiement Facture F110001" , ecritureComptable.getLibelle());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getEcritureComptableByRefNotFound() throws NotFoundException {
+        EcritureComptable ecritureComptable = getDaoProxy().getComptabiliteDao().getEcritureComptableByRef("AA-2015/00002");
     }
 
     @Test
