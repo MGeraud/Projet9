892c04884343ce0236d6ecbaf8984563339abd26
MGeraud <geraud.mon@gmail.com>
2021-01-07 16:07:35 +0100

    Add INSERT/UPDATE/DELETE tests to ComptabiliteDaoImplTest

--- src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -3,11 +3,16 @@ package com.dummy.myerp.consumer.dao.impl.db.dao;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.technical.exception.NotFoundException;
 import com.dummy.myerp.testconsumer.consumer.ConsumerTestCase;
+import org.apache.commons.lang3.ObjectUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -15,6 +20,17 @@ import static org.junit.Assert.*;
 public class ComptabiliteDaoImplTest extends ConsumerTestCase {
 
 
+    private EcritureComptable ecritureComptable;
+
+    @Before
+    public void setUp(){
+
+        ecritureComptable = new EcritureComptable();
+        ecritureComptable.setLibelle("Equilibrée");
+        ecritureComptable.setReference("AC-2019/00001");
+        ecritureComptable.setJournal(new JournalComptable("AC" , "Achat"));
+        ecritureComptable.setDate(new Date());
+    }
 
     @Test
     public void getListCompteComptable() {
@@ -56,19 +72,24 @@ public class ComptabiliteDaoImplTest extends ConsumerTestCase {
         EcritureComptable ecritureComptable = getDaoProxy().getComptabiliteDao().getEcritureComptableByRef("AA-2015/00002");
     }
 
+
     @Test
-    public void loadListLigneEcriture() {
+    public void insertEcritureComptable() throws NotFoundException {
+        getDaoProxy().getComptabiliteDao().insertEcritureComptable(ecritureComptable);
+        assertNotNull(getDaoProxy().getComptabiliteDao().getEcritureComptableByRef("AC-2019/00001"));
     }
 
     @Test
-    public void insertEcritureComptable() {
+    public void updateEcritureComptable() throws NotFoundException {
+        EcritureComptable toUpdate = getDaoProxy().getComptabiliteDao().getEcritureComptableByRef("AC-2019/00001");
+        toUpdate.setLibelle("updated");
+        getDaoProxy().getComptabiliteDao().updateEcritureComptable(toUpdate);
+        assertEquals( "updated", getDaoProxy().getComptabiliteDao().getEcritureComptableByRef("AC-2019/00001").getLibelle() );
     }
 
-    @Test
-    public void updateEcritureComptable() {
-    }
-
-    @Test
-    public void deleteEcritureComptable() {
+    @Test(expected = NotFoundException.class)
+    public void deleteEcritureComptable() throws NotFoundException {
+        getDaoProxy().getComptabiliteDao().deleteEcritureComptable(1);
+        getDaoProxy().getComptabiliteDao().getEcritureComptable(1);
     }
 }
