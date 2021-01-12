7528d56cea30459f251bf8d80db2caf5a2567c24
MGeraud <geraud.mon@gmail.com>
2021-01-12 15:07:16 +0100

    Refactor ComptabiliteManagerImplIntegrationTest.java

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerImplIntegrationTest.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerImplIntegrationTest.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class ComptabiliteManagerImplIT extends BusinessTestCase{
+public class ComptabiliteManagerImplIntegrationTest extends BusinessTestCase{
 
     private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
     private EcritureComptable ecritureComptable;
